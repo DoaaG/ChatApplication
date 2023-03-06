ffa455ed1e483e5005aaab32138078deb3fee5a9
DoaaAbdo <doaaa9073@gmail.com>
2023-03-06 15:59:10 +0200

    FireStore

---
+++ app/src/main/java/com/example/chatapplication/home/RoomData.kt
@@ -0,0 +1,5 @@
+package com.example.chatapplication.home
+
+data class RoomData(
+    var category : String
+)
--- app/src/main/java/com/example/chatapplication/login/LogInViewModel.kt
+++ app/src/main/java/com/example/chatapplication/login/LogInViewModel.kt
@@ -2,8 +2,11 @@ package com.example.chatapplication.login
 
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import com.example.chatapplication.UserProvider
 import com.example.chatapplication.base.BaseNavigator
 import com.example.chatapplication.base.BaseViewModel
+import com.example.chatapplication.database.FireStoreUtils
+import com.example.chatapplication.database.models.User
 import com.example.chatapplication.isValidEmail
 import com.example.chatapplication.register.RegisterNavigator
 import com.google.firebase.auth.FirebaseAuth
@@ -13,7 +16,7 @@ import com.google.firebase.ktx.Firebase
 class LogInViewModel : BaseViewModel<LogInNavigator>()  // for base functions
 {
     private var auth: FirebaseAuth = Firebase.auth
-    var loginNavigator : LogInNavigator? = null  // for navigating through pages
+    var loginNavigator: LogInNavigator? = null  // for navigating through pages
 
     var email = ObservableField<String?>()
     var password = ObservableField<String?>()
@@ -30,8 +33,7 @@ class LogInViewModel : BaseViewModel<LogInNavigator>()  // for base functions
         auth.signInWithEmailAndPassword(email.get()!!, password.get()!!)
             .addOnCompleteListener {
                 if (it.isSuccessful) {
-                    navigateToHome()
-                    navigator?.showMessage("Successful Login")
+                    getUsersFromDataBase(it.result.user?.uid)
 
                 } else {
                     navigator?.showMessage("${it.exception?.message}")
@@ -40,6 +42,21 @@ class LogInViewModel : BaseViewModel<LogInNavigator>()  // for base functions
 //        loginNavigator?.hide()
 
     }
+
+    private fun getUsersFromDataBase(uid: String?) {
+        FireStoreUtils().getUserFromDataBase(uid!!).addOnCompleteListener {
+            navigator?.hide()
+        if (it.isSuccessful) {
+            val user = it.result.toObject(User::class.java)
+            UserProvider.user = user
+            navigateToHome()
+            navigator?.showMessage("Successful Login")
+        } else {
+            navigator?.showMessage(it.exception?.localizedMessage ?: "")
+        }
+
+    }
+}
 
     fun createNewAccount() {
         loginNavigator?.navigateToRegister()
